c43eb21b0ac9ab57299170b7ae0fc1763a762c82
Mike Plis <mplis@oyster.com>
2017-08-22 13:44:54 -0400

    render specific routes with razzle instead of passing them through

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
+import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
 
 const App = () =>
   <Switch>
+    <Route exact path="/test" component={Home} />
     <Route exact path="/about" component={Home} />
   </Switch>;
 
--- src/server.js
+++ src/server.js
@@ -9,12 +9,43 @@ import request from 'request';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-// https://stackoverflow.com/questions/10435407/proxy-with-express-js
+const reactRender = (req, res) => {
+    const context = {};
+    const markup = renderToString(
+        <StaticRouter context={context} location={req.url}>
+            <App />
+        </StaticRouter>
+    );
+    if (context.url) {
+        res.redirect(context.url);
+    } else {
+        res.status(200).send(
+            `<!doctype html>
+        <html lang="">
+        <head>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta charSet='utf-8' />
+            <title>Welcome to Razzle</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
+            <script src="${assets.client.js}" defer></script>
+        </head>
+        <body>
+            <div id="root">${markup}</div>
+        </body>
+    </html>`
+        );
+    }
+};
+
+// These routes also need to be handled on the client side in App.js
+const reactRoutes = ['/test', '/about'];
 
 const server = express();
 server
     .disable('x-powered-by')
     .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+    .get(reactRoutes, reactRender)
     .get('/webpack/*', (req, res) => {
         const newUrl = 'http://localhost:8082' + req.url;
         console.log(newUrl);
@@ -23,32 +54,6 @@ server
     .get('/*', (req, res) => {
         const newUrl = 'http://localhost:8080' + req.url;
         request(newUrl).pipe(res);
-        //     const context = {};
-        //     const markup = renderToString(
-        //         <StaticRouter context={context} location={req.url}>
-        //             <App />
-        //         </StaticRouter>
-        //     );
-        //     if (context.url) {
-        //         res.redirect(context.url);
-        //     } else {
-        //         res.status(200).send(
-        //             `<!doctype html>
-        //     <html lang="">
-        //     <head>
-        //         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        //         <meta charSet='utf-8' />
-        //         <title>Welcome to Razzle</title>
-        //         <meta name="viewport" content="width=device-width, initial-scale=1">
-        //         ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
-        //         <script src="${assets.client.js}" defer></script>
-        //     </head>
-        //     <body>
-        //         <div id="root">${markup}</div>
-        //     </body>
-        // </html>`
-        //         );
-        //     }
     });
 
 export default server;
