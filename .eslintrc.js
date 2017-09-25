880d1b75e4b72bfd0427865a04470f14dc549f13
Mike Plis <mplis@oyster.com>
2017-09-25 16:49:18 -0400

    clean up eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,6 @@ module.exports = {
         jest: true
     },
     rules: {
-        // // Overwriting indentation rules to be 4 spaces instead of 2
-        // 'indent': ['error', 4],
-        // 'react/jsx-indent': ['error', 4],
-        // 'react/jsx-indent-props': ['error', 4],
-
         indent: ['error', 4],
         // 'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props': ['error', 4],
@@ -33,26 +28,6 @@ module.exports = {
         'react/jsx-closing-bracket-location': 'off',
         'react/jsx-wrap-multilines': 'off',
         'arrow-body-style': 'off',
-        'react/jsx-indent': 'off',
-
-        // These are all style rules that are errors in the airbnb config that result in too much noise in our current files
-        // my preference would be to eventually move most of them back to errors
-        'prefer-template': 'warn',
-        semi: 'warn',
-        'object-curly-spacing': 'warn',
-        quotes: 'warn',
-        'react/jsx-tag-spacing': 'warn',
-        // 'react/jsx-indent': 'warn',
-        'react/jsx-first-prop-new-line': 'warn',
-        'react/self-closing-comp': 'warn',
-        'jsx-quotes': 'warn',
-        'no-trailing-spaces': 'warn',
-
-        // I'd prefer if this was an error, but it's possible that there are edge cases right now that depend on some
-        // weird behavior of == so I'm changing to a warning for now
-        eqeqeq: 'warn',
-
-        // unless a11y is a priority for us, it's probably fine to set these rules as warnings
-        'jsx-a11y/no-static-element-interactions': 'warn'
+        'react/jsx-indent': 'off'
     }
 };
--- src/Home.js
+++ src/Home.js
@@ -3,33 +3,27 @@ import logo from './react.svg';
 import './Home.css';
 
 class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Oyster</h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          or
-          {' '}
-          <code>src/Home.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li><a href="https://palmer.chat">Community Slack</a></li>
-        </ul>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Home">
+                <div className="Home-header">
+                    <img src={logo} className="Home-logo" alt="logo" />
+                    <h2>Welcome to Oyster</h2>
+                </div>
+                <p className="Home-intro">
+                    To get started, edit <code>src/App.js</code> or <code>src/Home.js</code> and save to reload.
+                </p>
+                <ul className="Home-resources">
+                    <li>
+                        <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
+                    </li>
+                    <li>
+                        <a href="https://palmer.chat">Community Slack</a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Home;
